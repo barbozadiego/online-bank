d57882e632a70248f288116227906ec60f4cc867
Diego Barboza <barbozadiego1994@gmail.com>
2022-03-22 03:55:41 -0400

    Code refactoring and implementation of localstorage

--- scripts/index.js
+++ scripts/index.js
@@ -1,268 +1,297 @@
-
-const usuariosRegistrados = []
-
-//==========================================/* CLASES */=========================================================//
+/*_______________________________________________________________| Class  |_______________________________________________________________*/
 
 
 class User {
-    constructor(nombre,edad){
-        this.nombre = nombre
-        this.edad = edad
-        this.saldo
-        this.cuenta    
+    constructor(name,age){
+        this.name = name
+        this.age = age
+        this.balance
+        this.account    
     }
 } 
 
-//===================================================================================================//
+
+/*_______________________________________________________________| Variables  |_______________________________________________________________*/
+
+
+const displayOnScreen = document.getElementById('alerts'),
+      informationBox = document.createElement('div')
+      informationBox.className = ('information-box')
+
+
+let localUsers
+
+    window.localStorage.getItem('users')
+        ? localUsers = JSON.parse(window.localStorage.getItem('users'))
+        : localUsers = []
+
+    // if(window.localStorage.getItem('users')) {
+    //     localUsers = JSON.parse(window.localStorage.getItem('users'))
+    // } else {
+    //     localUsers = []
+    // }
 
 
 
-let imprimir = document.getElementById('alerts')
-let boxInfo = document.createElement('div')
-    boxInfo.className = ('info')
 
 
-//============================================/* Crear Cuenta */=======================================================//
+const formCreateAccount = document.querySelector('.create-account form')
+      formTransfer = document.querySelector('.transfer form')
+      formDeposit = document.querySelector('.deposit form')
+      formCheckBalance = document.querySelector('.check-balance form')
+      btnUserQuery = document.getElementById('userQuery')
 
-let formCrearCuenta = document.querySelector('.create-account form')
 
-formCrearCuenta.addEventListener('submit', (e) => {
-    let nombre = document.getElementById('nombre').value.toLowerCase().trim()
-    let edad = document.getElementById('edad').value
-        edad = parseInt(edad,10)
-    let montoApertura = document.getElementById('montoApertura').value
-        montoApertura = parseInt(montoApertura,10)
+
+/*_______________________________________________________________| Eventos Form  |_______________________________________________________________*/
+
+
+/*__________________| Create Account  |__________________*/
+
+    formCreateAccount.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const name = document.getElementById('name').value.toLowerCase().trim(),
+              age = parseInt(document.getElementById('age').value, 10),
+              startingAmount = parseInt(document.getElementById('startingAmount').value, 10)
+
+        createAccount(name,age,startingAmount)
+        displayOnScreen.appendChild(informationBox)
+    }) 
+
+
+/*__________________| Trasnfer Balance  |__________________*/
+
+    formTransfer.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      const remitent = document.getElementById('remitent').value.toLowerCase().trim(),
+            destinatary = document.getElementById('destinatary').value.toLowerCase().trim(),
+            account = parseInt(document.getElementById('accountNumber').value.trim(), 10),
+            amount = parseInt(document.getElementById('amountToSend').value.trim(), 10)
+
+        transfer(remitent,destinatary,account,amount) 
+        displayOnScreen.appendChild(informationBox)
+    })
+
+/*__________________| Deposit Funds  |__________________*/
+
+    formDeposit.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const name = document.getElementById('userName').value.toLowerCase().trim(),
+              account = parseInt(document.getElementById('accountToDeposit').value, 10),
+              amount = parseInt(document.getElementById('balanceToDeposit').value, 10)
+
+        deposit(name,account,amount)
+        displayOnScreen.appendChild(informationBox)
+    })
+
+/*__________________| Check Balance  |__________________*/
+
+    formCheckBalance.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const account = parseInt(document.getElementById('accountNumberToConsult').value, 10)
+
+        checkBalance(account)
+        displayOnScreen.appendChild(informationBox)
+    })
+
+/*__________________| Show Users  |__________________*/
+
+    btnUserQuery.addEventListener('click',() => {
+
+        // displayOnScreen.removeChild(informationBox)
+
+    const boxRegistrados = document.createElement('div'), 
+          eliminarDuplicado = document.getElementById('registro')
+          boxRegistrados.id = "registro"
+
+        if(localUsers) {
+            for(let user of localUsers) {
+                boxRegistrados.innerHTML += 
+                    `<p>
+                        <strong>👤 Name:</strong> ${user.name} | 
+                        <strong> Age:</strong> ${user.age} |
+                        <strong> N° Account:</strong> N°${user.account}
+                    </p>`
+            }
     
-    e.preventDefault()
+            if(eliminarDuplicado) displayOnScreen.removeChild(eliminarDuplicado)    
+            displayOnScreen.appendChild(boxRegistrados)
+        }
 
-    console.log(nombre, edad, montoApertura)
-
-    crearCuenta(nombre,edad,montoApertura)
-    imprimir.appendChild(boxInfo)
-
-}) 
-
-//============================================/* Realizar Transferencias */=======================================================//
-
-
-let formTransferir = document.querySelector('.transfer form')
-
-formTransferir.addEventListener('submit', (e) => {
-    let emisor = document.getElementById('emisor').value.toLowerCase().trim()
-    let receptor = document.getElementById('receptor').value.toLowerCase().trim()
-    let cuenta = document.getElementById('numeroCuenta').value.trim()
-    cuenta = parseInt(cuenta,10)
-    let monto = document.getElementById('montoTransferir').value.trim()
-    monto = parseInt(monto,10)
-    
-    e.preventDefault()
-
-   transferir(emisor,receptor,cuenta,monto) 
-    
-    imprimir.appendChild(boxInfo)
-})
+    })
 
 
 
 
-//==========================================/* Depositar */=========================================================//
-
-let formDeposito = document.querySelector('.deposit form')
-
-formDeposito.addEventListener('submit', (e) => {
-    let nombre = document.getElementById('nombreUsuario').value.toLowerCase().trim()
-    let cuenta = document.getElementById('cuentaDepositar').value.trim()
-        cuenta = parseInt(cuenta,10)
-    let monto = document.getElementById('montoSaldo').value.trim()
-        monto = parseInt(monto,10)
-
-    e.preventDefault()
-    depositar(nombre,cuenta,monto)
-    imprimir.appendChild(boxInfo)
-
-})
+/*_______________________________________________________________| Funciones  |_______________________________________________________________*/
 
 
-//==========================================/* Funcion Crear Cuenta */=========================================================//
+/*__________________| Create Account  |__________________*/
 
-
-const crearCuenta = (nombre,edad,montoApertura) => {
-    if (encontrarUsuario(nombre)) {
-        boxInfo.innerHTML = `
-                <p>❌ Usuario duplicado</p><br>
-                <p>El usuario que intenta registrar ya se encuentra en nuestra base de datos</p>
+const createAccount = (name,age,startingAmount) => {
+    if (findUsers(name)) {
+        informationBox.innerHTML = `
+                <p>⚠️ Duplicate user</p><br>
+                <p>The user you are trying to register is already in our database</p>
                 `
     } else {
-        if(edad >= 18 && montoApertura >= 100) {
+        if(age >= 18 && startingAmount >= 100) {
+
 
             for(let i= 1000; i <= 10000; i++) {
                 let cta = Math.ceil(Math.random()*10000)
                 if(cta > 1000 && cta < 10000) {
-                    if(cta !== usuariosRegistrados.cuenta){
-                    numCuenta = cta
+                    if(cta !== localUsers.account){
+                        accountNumber = cta
                     } break
                 } 
             }
-                let usuarioNuevo = new User(nombre,edad)
-                usuarioNuevo.saldo = montoApertura
-                usuarioNuevo.cuenta = numCuenta
-                boxInfo.innerHTML = `
-                    <p>✅ Cuenta creada exitosamente</p><br>
-                    <p>Sr(a) <strong>${nombre} </strong> su código de cuenta cliente es <strong>N°${numCuenta}</strong></p>
-                    `
-                usuariosRegistrados.push(usuarioNuevo)
-                formCrearCuenta.reset()
-        } else if (edad >= 18 && montoApertura < 100) {
             
-                boxInfo.innerHTML = `
-                    <p>❌ Monto incorrecto</p><br>
-                    <p>Estimado usuario, el monto minimo para aperturar una cuenta es de <strong>100$</strong></p>
+            let newUser = new User(name,age)
+                newUser.balance = startingAmount
+                newUser.account = accountNumber
+
+            informationBox.innerHTML = `<p>✅ Account created successfully</p><br>
+                    <p>Mr(s) <strong>${name} </strong> your customer account number is <strong>N°${accountNumber}</strong></p>
+                `
+
+            localUsers = [...localUsers, newUser]
+            window.localStorage.setItem('users', JSON.stringify(localUsers))
+
+            
+            formCreateAccount.reset()
+
+        } else if (age >= 18 && startingAmount < 100) {
+            
+                informationBox.innerHTML = `
+                    <p>❌ Wrong amount</p><br>
+                    <p>Dear user, the minimum amount to open an account is <strong>100$</strong></p>
                     `
-                    document.getElementById('edad').style.border = '.17em solid #cf1b1b'  ////////////////////////////////////////////////////////////////////
+                    document.getElementById('age').style.border = '.17em solid #cf1b1b'  ////////////////////////////////////////////////////////////////////
         } else {
-                boxInfo.innerHTML = `
-                   <p>❌ Eres menor de edad</p><br>
-                   <p>Estimado usuario no tienes edad suficiente para crearte una cuenta</p>
+                informationBox.innerHTML = `
+                   <p>❌ You are underage</p><br>
+                   <p>Dear user, you are not old enough to create an account</p>
                    `
         }
     }
 }
 
+/*__________________| Find User  |__________________*/
 
-//==========================================/* Funcion Encontrar Usuario */=========================================================//
-
-
-const encontrarUsuario = (nameUser) => {
-    let user
-    usuariosRegistrados.forEach(({nombre,edad,saldo,cuenta}, index) => {
-        if(nameUser === nombre) {
-            user = {
-                nombre, edad, saldo, cuenta, index
-              }
-        }
-    })
-    return user
+const findUsers = (nameUser) => {
+    if(localUsers) {
+        let user
+        localUsers.forEach(({name,age,balance,account}, index) => {
+            if(nameUser === name) {
+                user = { name, age, balance, account, index }
+            }
+        })
+        return user
+    }
 }
 
 
-//==========================================/* Funcion Transferir */=========================================================//
+/*__________________| Trasnfer  |__________________*/
 
-
-const transferir = (emisor,receptor,cuenta,monto) => { 
+const transfer = (remitent,destinatary,account,amount) => { 
  
 
-if(encontrarUsuario(emisor).nombre && encontrarUsuario(receptor).nombre ) {
-        if(encontrarUsuario(receptor).cuenta === cuenta && encontrarUsuario(emisor).saldo >= monto)  {  
-            let saldoDisponible = usuariosRegistrados[encontrarUsuario(emisor).index].saldo -= monto
+if(findUsers(remitent).name && findUsers(destinatary).name ) {
+        if(findUsers(destinatary).account === account && findUsers(remitent).balance >= amount)  {  
+            let saldoDisponible = localUsers[findUsers(remitent).index].balance -= amount
             
-            boxInfo.innerHTML = `
+            informationBox.innerHTML = `
             <p>✅ Transacción exitosa!</p><br>
             <p>saldo despues de la operación: <strong>${saldoDisponible}$</strong></p><br>
-            <p>Usted ha transferido <strong>${monto}$</strong> a la cuenta <strong>N°${cuenta}</strong></p>
+            <p>Usted ha transferido <strong>${amount}$</strong> a la cuenta <strong>N°${account}</strong></p>
             `
-            usuariosRegistrados[encontrarUsuario(receptor).index].saldo += monto
-            formTransferir.reset()
+            localUsers[findUsers(destinatary).index].balance += amount
+            formTransfer.reset()
 
-        } else if (encontrarUsuario(receptor).cuenta === cuenta && encontrarUsuario(emisor).saldo < monto) {
-            boxInfo.innerHTML = `
+        } else if (findUsers(destinatary).account === account && findUsers(remitent).balance < amount) {
+            informationBox.innerHTML = `
             <p>❌ Saldo insuficiente para esta operación</p>
             `
         } else {
-            boxInfo.innerHTML = `
+            informationBox.innerHTML = `
             <p>❌ Numero de cuenta invalido</p>
             `
         } 
 
 } else {
-    boxInfo.innerHTML = `
+    informationBox.innerHTML = `
             <p>❌ Usuario no existe</p>
             `
 }
 }
 
+/*__________________| Deposit  |__________________*/
 
-//==========================================/* Funcion Depositar */=========================================================//
+const deposit = (name,accountNumber,amount) => {
 
-
-const depositar = (nombre,cuenta,monto) => {
-        let usuario = encontrarUsuario(nombre) 
-    if(usuario.cuenta === cuenta) {
-        usuariosRegistrados[usuario.index].saldo += monto
-        boxInfo.innerHTML = `
-            <p>✅ Transacción efectiva</p><br>
-            <p>Usted a depositado la cantidad de <strong>${monto}$</strong> a la cuenta <strong>N°${cuenta}</strong> perteneciente a <strong>${nombre}</strong></p>
-            `
-        formDeposito.reset()
+    if(findUsers(name)) {
+        const {account, index} = findUsers(name) 
+    
+        if(account === accountNumber) {
+            localUsers[index].balance += amount
+            informationBox.innerHTML = 
+               `
+                <p>✅ Succesful transaction</p><br>
+                <p>You have deposited the amount of <strong>${amount}$</strong> to the account <strong>N°${accountNumber}</strong> belonging to <strong>${name}</strong></p>
+               `
+        } 
     } else {
-        boxInfo.innerHTML = `
-            <p>❌ Datos incorrectos</p><br>
-            <p>Estimado usuario los datos que ha ingresado son incorrectos, porfavor verifique e intente nuevamente</p>
-            `
+        informationBox.innerHTML = 
+            `<p>❌ Wrong data</p><br>
+             <p>Dear user, the data you have entered is incorrect, please check and try again</p>`
     }
+    formDeposit.reset()
 }
 
 
 
-//==========================================/* Funcion Ver Usuarios Registrados */=========================================================//
+/*__________________| Check Balance  |__________________*/
 
 
-
-let btnRegistrados = document.getElementById('userQuery')
-
-btnRegistrados.addEventListener('click',() => {
-
-    // imprimir.removeChild(boxInfo)
-
-   const boxRegistrados = document.createElement('div') 
-   const eliminarDuplicado = document.getElementById('registro')
-    boxRegistrados.id = "registro"
-    for(let usuario of usuariosRegistrados) {
-        // <p>👤 Usuarios Registrados:</p><br>
-        // boxRegistrados.innerHTML = `Usuarios Registrados:`
-        // boxRegistrados.innerHTML = `Nombre  |  Edad    |   Cuenta`
-        boxRegistrados.innerHTML += `
-        <p><strong>👤 Nombre:</strong> ${usuario.nombre} | <strong>Edad:</strong> ${usuario.edad} | <strong>Cuenta:</strong> N°${usuario.cuenta}</p>
-        `
+const findAccount = (accountNumber) => {
+    
+    if(localUsers) {
+        let user
+        localUsers.forEach(({name,age,balance,account}, index) => {
+            if(accountNumber === account) {
+                user = { name, age, balance, account, index}
+            }
+        })
+        return user
     }
-    if(eliminarDuplicado) imprimir.removeChild(eliminarDuplicado)    
-    imprimir.appendChild(boxRegistrados)
-})
-
-
-
-//==========================================/* Funcion consultar Saldo */=========================================================//
-
-let formConsultarSaldo = document.getElementById('form-consultarSaldo')
-
-formConsultarSaldo.addEventListener('click', (e) => {
-    let nombre = document.getElementById('usuarioNombre').value.toLowerCase().trim()
-
-    e.preventDefault()
-    consultarSaldo(nombre)
-    imprimir.appendChild(boxInfo)
-
-})
-
-const consultarSaldo = (nombre) => {
-
-    if(encontrarUsuario(nombre)){
-    let saldo = encontrarUsuario(nombre).saldo
-    boxInfo.innerHTML = `
-        <p>💰 Saldo Disponible</p><br>
-        <p>Estimado(a) <strong>${nombre}</strong> el saldo disponible en su cuenta es de <strong>${saldo}$</strong></p>
-       `
-       formConsultarSaldo.reset()
-
-    } else if (nombre === '') {
-        boxInfo.innerHTML = `
-        <p>⚠️ Ingrese un nombre para poder consultar su saldo</p>
-        `
-    } else  {
-        boxInfo.innerHTML = `
-        <p>❌ El usuario al que intenta depositar no posee cuenta en nuestro banco</p>
-        `
-    }   
 }
+
+const checkBalance = (accountNumber) => {
+    if(findAccount(accountNumber)) {
+        const {name, balance, account} = findAccount(accountNumber)
+    
+        if(accountNumber === account) {
+             informationBox.innerHTML = 
+                `
+                <p>💸 Available Balance</p><br>
+                <p>Dear <strong>${name},</strong> the available balance in your account is <strong>${balance} $</strong></p>
+                `
+           formCheckBalance.reset()
+        } 
+    }
+    else if (isNaN(accountNumber)) informationBox.innerHTML = `<p>⚠️ Enter a valid account number to check the available balance</p>`
+    else informationBox.innerHTML =  `<p>❌ The account number you have entered is not registered in our banking system.</p>`
+
+    formCheckBalance.reset()
+}
+
+
+
 
 
 
